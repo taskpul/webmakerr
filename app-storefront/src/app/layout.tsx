f37a09ee15fd7f8fabf09d960cd59b5abb7e7609
taskpul <romtietz@gmail.com>
2025-09-08 08:54:42 +0200

    feat(app-storefront): add Facebook Pixel tracking

--- app-storefront/src/app/layout.tsx
+++ app-storefront/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import "styles/globals.css"
+import FacebookPixel from "@modules/analytics/components/facebook-pixel"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -10,6 +11,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <FacebookPixel />
         <main className="relative">{props.children}</main>
       </body>
     </html>
