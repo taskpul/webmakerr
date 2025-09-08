6428f6974012f10f02fec550271dc755e01fdb60
taskpul <romtietz@gmail.com>
2025-09-08 09:35:36 +0200

    feat(app-storefront): add case study section to home

--- app-storefront/src/app/[countryCode]/(main)/page.tsx
+++ app-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import Features from "@modules/home/components/features"
+import CaseStudy from "@modules/home/components/case-study"
 import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
@@ -33,6 +34,7 @@ export default async function Home(props: {
     <>
       <Hero />
       <Features />
+      <CaseStudy />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
