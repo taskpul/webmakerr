2b1b77880ba149f588a559e9165dc43bba7bcb75
taskpul <romtietz@gmail.com>
2025-09-12 14:03:33 +0200

    Revert "fix(app-storefront): apply consistent h2 line-height"

--- app-storefront/src/modules/account/components/order-overview/index.tsx
+++ app-storefront/src/modules/account/components/order-overview/index.tsx
@@ -27,7 +27,7 @@ const OrderOverview = ({ orders }: { orders: HttpTypes.StoreOrder[] }) => {
       className="w-full flex flex-col items-center gap-y-4"
       data-testid="no-orders-container"
     >
-      <h2 className="text-large-semi leading-10">Nothing to see here</h2>
+      <h2 className="text-large-semi">Nothing to see here</h2>
       <p className="text-base-regular">
         You don&apos;t have any orders yet, let us change that {":)"}
       </p>
